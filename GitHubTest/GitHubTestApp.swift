14eaad639e8cdbbbc5e733413f355f4a8469215e
LeeSeonQ <lsk021181@gmail.com>
2023-07-12 18:53:47 +0900

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Lee on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
